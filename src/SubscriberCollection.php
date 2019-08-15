55b7cc8bd73a84c2b421712c2ff6d6713bee8d90
Ben Brumm <ben@databasestar.com>
2019-08-15 20:08:15 +1000

    Fixed phpunit tests

---
+++ src/SubscriberCollection.php
@@ -0,0 +1,12 @@
+<?php
+
+class SubscriberCollection {
+
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+}
--- tests/sample_test.php
+++ tests/sample_test.php
@@ -1,4 +1,5 @@
 <?php
+require_once("src/SubscriberCollection.php");
 
 class SampleTest extends \PHPUnit\Framework\TestCase
 {
@@ -10,6 +11,18 @@ class SampleTest extends \PHPUnit\Framework\TestCase
     public function testSomething() {
 
         $this->assertEquals(1, 1);
+    }
+
+    public function testAdd() {
+        $a = 1;
+        $b = 2;
+        $subscriberCollection = new SubscriberCollection();
+
+        $expected = 3;
+        $actual = $subscriberCollection->add($a, $b);
+
+        $this->assertEquals($expected, $actual);
+
     }
 
 }
