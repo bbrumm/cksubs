635d1795c86a5a87296003a7e1cc6870098d2ed5
Ben Brumm <ben@databasestar.com>
2019-08-18 14:44:45 +1000

    Added DB creation and load

---
+++ data_setup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE subscriber (
+  subscriber_id int(10) DEFAULT NULL,
+  first_name varchar(200) DEFAULT NULL,
+  email_address varchar(320) DEFAULT NULL,
+  subscriber_state varchar(100) DEFAULT NULL,
+  subscriber_created_at date DEFAULT NULL,
+  KEY idx_sub_id (subscriber_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
