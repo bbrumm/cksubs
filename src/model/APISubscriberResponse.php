d7169103796c6cbc94a97da31963b6d8d26a41f5
Ben Brumm <ben@databasestar.com>
2019-08-19 20:56:13 +1000

    Echo the ENV variable

--- src/model/APISubscriberResponse.php
+++ src/model/APISubscriberResponse.php
@@ -9,6 +9,8 @@ class APISubscriberResponse implements ISubscriberResponse {
     }
 
     public function getPageOfSubscribers($pageNumber) {
+        echo "ENV: ";
+        print_r($_ENV);
         $apiKey = $_ENV["CK_API_KEY"];
         $apiSecretKey = $_ENV["CK_API_SECRET"];
         $ck = new \ConvertKit\ConvertKit($apiKey, $apiSecretKey);
